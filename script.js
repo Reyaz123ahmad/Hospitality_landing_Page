18a3897d5667fd6c43c757d2b5da4726914a43c6
Reyaz Ahmad <reyazahmad40544@gmail.com>
2025-03-30 15:15:20 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,71 @@
+// const call =document.getElementsByClassName('call')
+// call.addEventListener('click',function(){
+//      window.location.href='tel:0123 222 333'
+// })
+// const map=document.querySelector(".map-btn");
+//     map.addEventListener('click',function(){
+//         console.log('he')
+           
+//     window.location.href='https://www.google.com/maps'
+// });
+
+
+document.querySelector(".call").addEventListener("click",function(){
+           
+    window.location.href='tel:0123 222 333'
+});
+document.querySelector(".footer-phone").addEventListener("click",function(){
+           
+    window.location.href='tel:+00 123-456-789'
+});
+
+document.querySelector(".footer-mail").addEventListener("click",function(){
+           
+    window.location.href='mailto:demo@example.com'
+});
+
+document.querySelector("#face1").addEventListener("click",function(){
+           
+    window.location.href='https://www.facebook.com'
+});
+document.querySelector("#tele1").addEventListener("click",function(){
+           
+    window.location.href='https://www.telegram.com'
+});
+
+document.querySelector("#insta1").addEventListener("click",function(){
+           
+    window.location.href='https://www.instagram.com'
+});
+document.querySelector("#youtub1").addEventListener("click",function(){
+           
+    window.location.href='https://www.youtube.com'
+});
+document.querySelector("#whats1").addEventListener("click",function(){
+           
+    window.location.href='https://www.whatsapp.com'
+});
+document.querySelector(".footer-location").addEventListener("click",function(){
+           
+    window.location.href='https://maps.app.goo.gl/ygxncq4J1V5cHkvb7'
+});
+document.querySelector(".location-mail").addEventListener("click",function(){
+           
+    window.location.href='mailto:demo@example.com'
+});
+document.querySelector(".location-phone").addEventListener("click",function(){
+           
+    window.location.href='tel: +00 123-456-789'
+});
+
+
+// const nav=document.querySelector('.header');
+// window.addEventListener('scroll',function(){
+//    if(window.scrollY>15){
+//     nav.style.backgroundColor='lightseagreen'
+//    }
+//    else{
+//      nav.style.backgroundColor='transparent'
+//    }
+// })
+    
